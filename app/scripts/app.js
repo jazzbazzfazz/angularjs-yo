91e0fb63fca07c54b2990a63339b3a0a42d97a8f
jazzbazzfazz <jazz@bazzfazz.com>
2016-07-30 20:12:15 -0500

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularjsYoApp
+ * @description
+ * # angularjsYoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularjsYoApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+]);
